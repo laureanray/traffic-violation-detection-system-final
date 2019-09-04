a7ac7a60de3b6c44c70603b63167f98b9d80b46d
lr <laureanraybahala@gmail.com>
2019-09-04 18:55:51 +0800

    Added ROI for traffic

--- widgets/detection_view.py
+++ widgets/detection_view.py
@@ -18,16 +18,17 @@ with tf.Session() as sess:
             # Load GUI
             uic.loadUi('./user_interface/DetectionView.ui', self)
             self.imageLabel.setScaledContents(True)
+            self.trafficLight.setScaledContents(True)
             self.capture = None
             self.setWindowTitle('Detection Window')
-            self.timer = QtCore.QTimer(self, interval=5)
+            self.timer = QtCore.QTimer(self, interval=50)
             self.timer.timeout.connect(self.update_frame)
             self.start_webcam()
 
         @QtCore.pyqtSlot()
         def start_webcam(self):
             if self.capture is None:
-                self.capture = cv.VideoCapture("/home/lr/Videos/vid.mp4", 0)
+                self.capture = cv.VideoCapture("/home/lr/Desktop/traffic_light.avi", 0)
                 self.capture.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
                 self.capture.set(cv.CAP_PROP_FRAME_WIDTH, 640)
             self.timer.start()
@@ -35,8 +36,20 @@ with tf.Session() as sess:
         @QtCore.pyqtSlot()
         def update_frame(self):
             ret, image = self.capture.read()
-            simage = cv.flip(image, 1)
-            self.displayImage(image, True)
+            resized = cv.resize(image, (1280, 720));
+            x = 299*2;
+            y = 181*2;
+            w = 10*2;
+            h = 18*2;
+            roi = resized[y:y+h, x:x+w]
+            cv.rectangle(resized, (x, y), (x + w, y + h), (255, 255, 00), 2)
+            # cv.imshow('roi', roi)
+            simage = cv.flip(resized, 1)
+            # print(resized.shape);
+            # print(roi.shape);
+            self.displayImage(resized, True, self.imageLabel)
+            print(self.getTrafficLightStatus(roi))
+            # self.displayImage(roi, True, self.trafficLight)
 
         @QtCore.pyqtSlot()
         def capture_image(self):
@@ -48,7 +61,7 @@ with tf.Session() as sess:
                 cv.imwrite(os.path.join(path, name), frame)
                 self._image_counter += 1
 
-        def displayImage(self, img, window=True):
+        def displayImage(self, img, window=True, label=None):
             qformat = QtGui.QImage.Format_Indexed8
             if len(img.shape) == 3:
                 if img.shape[2] == 4:
@@ -58,12 +71,41 @@ with tf.Session() as sess:
             outImage = QtGui.QImage(img, img.shape[1], img.shape[0], img.strides[0], qformat)
             outImage = outImage.rgbSwapped()
             if window:
-                self.imageLabel.setPixmap(QtGui.QPixmap.fromImage(outImage))
+                label.setPixmap(QtGui.QPixmap.fromImage(outImage))
 
-            # Uncommend this on deploy
+            # Uncomment this on deploy
             # self.showFullScreen()
 
             # self.show()
+        def getTrafficLightStatus(self, frame):
+            # 20 x 36
+            # Red ROI
+            red_x = 0
+            red_y = 0
+            red_w = 20
+            red_h = 12
+            # Amber ROI
+            amber_x = 0
+            amber_y = 12
+            amber_w = 20
+            amber_h = 12
+            # Green ROI
+            green_x = 0
+            green_y = 24
+            green_w = 20
+            green_h = 12
+            cv.rectangle(frame, (red_x, red_y), (red_x + red_w, red_y + red_h), (0, 0, 255), 1)
+            cv.rectangle(frame, (amber_x, amber_y), (amber_x + amber_w, amber_y + amber_h), (0, 255, 255), 1)
+            cv.rectangle(frame, (green_x, green_y), (green_x + green_w, green_y + green_h), (0, 255, 0), 1)
+            print(frame.shape)
+
+            red = frame[red_y:red_y+red_h, red_x:red_x+red_w]
+
+            print(red)
+            # cv.waitKey(0)
+            cv.imshow('frame', frame)
+            return "Test"
+
 
     # if __name__=='__main__':
     #     import sys
